bad95381ce6adf5661a290d50c8c8c2a2cfef8e6
ayadeleke <a.adeleke@alustudent.com>
2025-01-23 12:54:12 +0100

    session

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -23,26 +23,30 @@ elif getenv('AUTH_TYPE') == 'session_auth':
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
 
-# If AUTH_TYPE is not defined or has an unknown value, the `auth` variable will remain None.
+# If AUTH_TYPE is not defined or has an unknown value, abort.
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler """
     return jsonify({"error": "Not found"}), 404
 
+
 @app.errorhandler(401)
 def access_denied(error) -> str:
     """ Unauthorized handler """
     return jsonify({"error": "Unauthorized"}), 401
 
+
 @app.errorhandler(403)
 def access_forbidden(error) -> str:
     """ Forbidden handler """
     return jsonify({"error": "Forbidden"}), 403
+
 
 @app.before_request
 def before_request_handler():
@@ -50,6 +54,7 @@ def before_request_handler():
     excluded = ['/api/v1/status/',
                 '/api/v1/unauthorized/',
                 '/api/v1/forbidden/']
+
 
     if auth is not None and auth.require_auth(request.path, excluded):
         if auth.authorization_header(request) is None:
--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -5,8 +5,10 @@
 
 from api.v1.auth.auth import Auth
 
+
 class SessionAuth(Auth):
     """SessionAuth class that inherits from Auth.
-    For now, it doesn't add any extra functionality but can be extended in the future.
+    For now, it doesn't add any extra functionality
+    but can be extended in the future.
     """
     pass
