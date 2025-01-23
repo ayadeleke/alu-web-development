c4cd57c5e225eaf62436090bc25ebb3ff3fc6f01
ayadeleke <a.adeleke@alustudent.com>
2025-01-23 13:06:35 +0100

    session

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -2,13 +2,20 @@
 """ Module of Users views
 """
 
-
 from api.v1.auth.auth import Auth
-
+import uuid
 
 class SessionAuth(Auth):
     """SessionAuth class that inherits from Auth.
-    For now, it doesn't add any extra functionality
-    but can be extended in the future.
+    Now includes session management.
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a Session ID for a user_id.
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
