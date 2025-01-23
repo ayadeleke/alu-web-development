d05f6733ed278dbe9bb34f53038cf235b088ff29
ayadeleke <a.adeleke@alustudent.com>
2025-01-23 13:09:56 +0100

    session

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """ Module of Users views
 """
+from typing import Any
 
 from api.v1.auth.auth import Auth
 import uuid
-
 
 class SessionAuth(Auth):
     """SessionAuth class that inherits from Auth.
@@ -12,7 +12,7 @@ class SessionAuth(Auth):
     """
     user_id_by_session_id = {}
 
-    def create_session(self, user_id: str = None) -> str:
+    def create_session(self, user_id: str = None) -> str | None:
         """Creates a Session ID for a user_id.
         """
         if user_id is None or not isinstance(user_id, str):
@@ -20,3 +20,10 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> Any | None:
+        """Returns a User ID based on a Session ID.
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
