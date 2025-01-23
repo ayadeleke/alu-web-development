22e8db36323d57e415a5f24eb9a6c66800a4ea43
ayadeleke <a.adeleke@alustudent.com>
2025-01-23 13:22:08 +0100

    session

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -5,7 +5,7 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
+from flask_cors import CORS, cross_origin
 import os
 
 # Import SessionAuth if needed
@@ -53,10 +53,11 @@ def before_request_handler():
     """before_request handler"""
     excluded = ['/api/v1/status/',
                 '/api/v1/unauthorized/',
-                '/api/v1/forbidden/']
+                '/api/v1/forbidden/',
+                '/api/v1/auth_session/login/']
 
     if auth is not None and auth.require_auth(request.path, excluded):
-        if auth.authorization_header(request) is None:
+        if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
             abort(401)
 
         user = auth.current_user(request)
