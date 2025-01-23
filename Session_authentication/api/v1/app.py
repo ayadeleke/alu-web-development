0ceebeec5b9469a1e36d031175b9f96b3f7bbca6
ayadeleke <a.adeleke@alustudent.com>
2025-01-23 12:49:00 +0100

    session

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -8,42 +8,41 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 
-
-app = Flask(__name__)
-app.register_blueprint(app_views)
-CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+# Import SessionAuth if needed
 auth = None
 
 if getenv('AUTH_TYPE') == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
+
 elif getenv('AUTH_TYPE') == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
+elif getenv('AUTH_TYPE') == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
+
+# If AUTH_TYPE is not defined or has an unknown value, the `auth` variable will remain None.
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
-    """
+    """ Not found handler """
     return jsonify({"error": "Not found"}), 404
-
 
 @app.errorhandler(401)
 def access_denied(error) -> str:
-    """
-    Uh Uh Uh. You didnt say the magic word
-    """
+    """ Unauthorized handler """
     return jsonify({"error": "Unauthorized"}), 401
-
 
 @app.errorhandler(403)
 def access_forbidden(error) -> str:
-    """
-    No cookies for you
-    """
+    """ Forbidden handler """
     return jsonify({"error": "Forbidden"}), 403
-
 
 @app.before_request
 def before_request_handler():
---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+# api/v1/auth/session_auth.py
+
+from api.v1.auth.auth import Auth
+
+class SessionAuth(Auth):
+    """SessionAuth class that inherits from Auth.
+    For now, it doesn't add any extra functionality but can be extended in the future.
+    """
+    pass
