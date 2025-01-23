2e298fa6ae9effab785437542cf4aae43af3de44
ayadeleke <a.adeleke@alustudent.com>
2025-01-23 13:25:35 +0100

    session

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,8 @@
 
 from api.v1.auth.auth import Auth
 import uuid
+import os
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -27,3 +29,22 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def session_cookie(self, request=None):
+        """Returns a cookie value from a request.
+        """
+        if request is None:
+            return None
+        session_name = os.getenv('SESSION_NAME', '_my_session_id')
+        return request.cookies.get(session_name)
+
+    def current_user(self, request=None):
+        """Returns a User instance based on a cookie value.
+        """
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return None
+        return User.get(user_id)
