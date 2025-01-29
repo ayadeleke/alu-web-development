e51483cbfb03588da7e323c398026caa72cac25a
ayadeleke <a.adeleke@alustudent.com>
2025-01-29 19:51:55 +0100

    update_pass

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -133,3 +133,20 @@ class Auth:
             return reset_token
         except NoResultFound:
             raise ValueError("User does not exist")
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update user's password using reset token
+
+        Args:
+            reset_token (str): Reset token
+            password (str): New password
+
+        Returns:
+            None
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_password = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=hashed_password, reset_token=None)
+        except NoResultFound:
+            raise ValueError("Invalid reset token")
