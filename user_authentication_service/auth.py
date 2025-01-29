dcdbc221ac177e045feaa0f4e26f71233a8eaf4a
ayadeleke <a.adeleke@alustudent.com>
2025-01-29 19:43:50 +0100

    session_destroy

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -103,7 +103,6 @@ class Auth:
         except NoResultFound:
             return None
 
-
     def destroy_session(self, user_id: int) -> None:
         """Destroys a user's session
 
@@ -117,4 +116,3 @@ class Auth:
             self._db.update_user(user_id, session_id=None)
         except NoResultFound:
             pass
-
