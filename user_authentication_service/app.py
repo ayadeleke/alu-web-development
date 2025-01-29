6d3eae418ac66d93009e183972cbba62eb9f7865
ayadeleke <a.adeleke@alustudent.com>
2025-01-29 19:35:43 +0100

    login

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """Flask app"""
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, make_response
 from auth import Auth
-
 
 app = Flask(__name__)
 AUTH = Auth()
@@ -25,8 +24,25 @@ def create_user():
         user = AUTH.register_user(email, password)
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
-    return jsonify({"email": f"{email}", "message": "user created"})
+    return jsonify({"email": email, "message": "user created"})
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login():
+    """Login function"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if not email or not password:
+        abort(400)
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    response = make_response(jsonify({"email": email, "message": "logged in"}))
+    response.set_cookie("session_id", session_id)
+    return response
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port=5000)
